d5528f3ee08388fc5cd8fcabe1c65174ea0a685a
Robin2389 <robin.riehn@gmail.com>
2019-04-16 12:31:11 +0200

    Update star.js to include spacex logo

--- star.js
+++ star.js
@@ -2,7 +2,8 @@
 
 Star[] stars = new Star[800];
 
-PImage img;
+PImage starimg;
+PImage spaceximg;
 float speed;
 
   
@@ -19,12 +20,22 @@ void setup() {
   }
   
   size(1000, 800);
-  img = loadImage("stars.jpg");
+  
+  /* 
+  here is where you load your images, make sure the file names are 
+  exactly the same to the ones in the folder where you downloaded 
+  your photos 
+  */
+  starimg = loadImage("stars.jpg");
+  spaceximg = loadImage("spacex.png"):
 }
 
 void draw() {
   speed = map(mouseX, 0, width, 0, 20);
-  image(img, 0, 0);
+  image(starimg, 0, 0);
+  spaceximg.resize(0, 200);  
+  image (spaceximg, 100, 300);  // location of spacex logo:
+                                // 100 pixels in x, 300 pixes in y
   
   translate(width/2, height/2);
   for (int i = 0; i < stars.length; i++) {
